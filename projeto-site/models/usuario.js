ef7b80a3739d8c0b4cc6fb7abbe37b7f89c3e9f8
lahtejon <lais.aranega@bandtec.com.br>
2020-12-02 11:08:33 -0300

    projeto finalizado

---
+++ projeto-site/models/usuario.js
@@ -0,0 +1,45 @@
+	'use strict';
+
+/* 
+lista e explicação dos Datatypes:
+https://codewithhugo.com/sequelize-data-types-a-practical-guide/
+*/
+
+module.exports = (sequelize, DataTypes) => {
+    let cadastro = sequelize.define('cadastro',{
+		id: {
+			field: 'idcadastro',
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+			autoIncrement: true
+		},		
+		nome: {
+			field: 'nome',
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		email: {
+			field: 'email',
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		senha: {
+			field: 'senha',
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		aniversario: {
+			field: 'aniversario',
+			type: DataTypes.STRING,
+			allowNull: false
+		}
+	}, 
+	{
+		tableName: 'cadastro', 
+		freezeTableName: true, 
+		underscored: true,
+		timestamps: false,
+	});
+
+    return cadastro;
+};
